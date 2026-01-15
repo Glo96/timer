a96a0b44f288c5202bfd313e855cdfd64c2e253f
Gloria Borisova <dark.alice.ray@gmail.com>
2026-01-15 20:11:46 +0200

    Add files via upload

---
+++ app.js
@@ -0,0 +1,167 @@
+let interval = [];
+let counter = [];
+    
+const startTimer = (event) => {
+    const startBtnID = event.target.id;
+    const timerID = parseInt(startBtnID);
+    const inputID = `${timerID}-timer-input`;
+
+    // Initialize Counter if it's 0 or less
+    if (counter[timerID] <= 0) {
+        counter[timerID] = document.getElementById(inputID).value;
+    }
+    // TODO: Alert for invalid input. Can not be empty or less than 0 or not a number.
+    if (counter[timerID] <= 0) {
+        return;
+        // alert("Please enter a valid number.")
+        // return
+    }
+
+    // Guard Clause For Start Button Spam
+    event.target.disabled = true;
+    
+    displayTimer(timerID); 
+
+    // Set Interval 
+    interval[timerID] = setInterval(() => {
+        counter[timerID]--;
+        console.log("Hi I am an interval and I am still working...");
+        
+        // Timer Display Update
+        displayTimer(timerID)
+        
+        // Clear/Stop Interval if the timer reaches 0;
+        if (counter[timerID] <= 0) {
+            stopTimer(event)
+        }
+    }, 1000)
+}
+
+const displayTimer = (timerID) => {
+    let seconds = counter[timerID] % 60;
+    let minutes = Math.floor(counter[timerID] / 60);
+    const timerDisplayID = `${timerID}-timer-display`;
+    
+    document.getElementById(timerDisplayID).innerHTML = `${String(minutes).padStart(2, "0")}:${String(seconds).padStart(2, "0")}`;
+}
+    
+const stopTimer = (event) => {
+    const stopBtnID = event.target.id;
+    
+    const timerID = parseInt(stopBtnID);
+    clearInterval(interval[timerID]);
+    document.getElementById(`${timerID}-startBtn`).disabled = false;
+}
+
+const resetTimer = (event) => {
+    const resetBtnID = event.target.id;
+    const timerID = parseInt(resetBtnID);
+    const timerDisplayID = `${timerID}-timer-display`;
+    const inputID = `${timerID}-timer-input`;
+    
+    clearInterval(interval[timerID]);
+    counter[timerID] = 0;
+    document.getElementById(timerDisplayID).innerHTML = "00:00";
+    document.getElementById(`${timerID}-startBtn`).disabled = false;
+    document.getElementById(inputID).value = "";
+}
+
+const removeDiv = (event) => {
+    const removeBtnID = event.target.id;
+    const timerID = parseInt(removeBtnID);
+    const timerDivID = `${timerID}-timer-div`;
+    const timerDiv = document.getElementById(timerDivID);
+     stopTimer(event)
+    timerDiv.remove();
+}    
+
+const syncAllTimers = () => {
+    const arrayWithBtnClassNames = document.querySelectorAll(".start-button");
+    
+    for (let i = 0; i < arrayWithBtnClassNames.length; i++) {
+        const timerIndex = parseInt(arrayWithBtnClassNames[i].id);
+
+        clearInterval(interval[timerIndex]);
+        if (counter[timerIndex] != 0) {
+            startTimer({target: {id: `${timerIndex}-startBtn`}});
+        }
+    }
+}
+
+// =============================== ADD NEW TIMER FUNCTION ===============================
+let idIndex = 0;
+const addNewTimer = () => {
+    counter.push(0);
+    interval.push(0);
+    // Div element
+    const newDiv = document.createElement("div");
+    Object.assign(newDiv, {
+        id: `${idIndex}-timer-div`,
+        className: "timer-div",
+    });
+   
+
+    const timerDisplay = document.createElement("h1");
+    const timerDisplayText = "00:00";
+    timerDisplay.id = `${idIndex}-timer-display`;
+    timerDisplay.className = "timer-display";
+    timerDisplay.append(timerDisplayText);
+
+    // Input Field type number
+    const inputField = document.createElement("input");
+    Object.assign(inputField, {
+        type: "number",
+        id: `${idIndex}-timer-input`,
+        placeholder: "Enter duration in seconds",
+    })
+    
+    // Start Button
+    const startBtn = document.createElement("button")
+    Object.assign(startBtn, {
+        id: `${idIndex}-startBtn`,
+        className: "btn start-button",
+    })
+    const textForStartBtn = document.createTextNode("Start Timer")
+    startBtn.appendChild(textForStartBtn)
+
+    // Stop Button
+    const stopBtn = document.createElement("button")
+    Object.assign(stopBtn, {
+        id: `${idIndex}-stopBtn`,
+        className: "btn stop-button",
+    })
+    const textForStopBtn = document.createTextNode("Stop Timer")
+    stopBtn.appendChild(textForStopBtn)
+
+    // Reset Button
+    const resetBtn = document.createElement("button")
+    Object.assign(resetBtn, {
+        id: `${idIndex}-resetBtn`,
+        className: "btn reset-button",
+    })
+    const textForResetBtn = document.createTextNode("Reset Timer")
+    resetBtn.appendChild(textForResetBtn);
+
+    //Remove Button
+    const removeBtn = document.createElement("button");
+    Object.assign(removeBtn, {
+        id: `${idIndex}-removeBtn`,
+        className: "btn remove-button",
+    });
+    const textForRemoveBtn = document.createTextNode("Remove Timer");
+    removeBtn.appendChild(textForRemoveBtn);
+    
+    // Clear Interval if running
+    // Add the elements to the div and then to the body
+    newDiv.append(timerDisplay, inputField, startBtn, stopBtn, resetBtn, removeBtn)
+    document.body.append(newDiv)
+    
+    startBtn.addEventListener("click", startTimer)
+    stopBtn.addEventListener("click", stopTimer)
+    resetBtn.addEventListener("click", resetTimer)
+    removeBtn.addEventListener("click", removeDiv)
+    
+    idIndex++;                 
+}
+
+// TODO: use delete for removing elements; use empty slots to include new elements and change index number with the index of the empty slot, if no empty slots - add element to end of the array
